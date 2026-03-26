e037d87ef14d372676cc6dfda15458ed42fa7828
ANKISH1 <ankishchaudhary51@gmail.com>
2026-03-26 14:14:08 +0530

    feat: add unit tests for auth and problems

--- backend/apps/problems/tests.py
+++ backend/apps/problems/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
-
+from rest_framework.test import APIClient
+from apps.users.models import User
+from django.urls import reverse
+from .models import Problems
 # Create your tests here.
+
+class ProblemCreateList(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.all_problems_url = reverse('problems')
+        self.user = User.objects.create_user(
+            email="user@test.com",
+            username="testuser",
+            password="testpassword"
+        )
+        self.admin = User.objects.create_superuser(
+            email="amdin@test.com",
+            username="adminuser",
+            password="adminpassword"
+        )
+        self.client.force_authenticate(user = self.user)
+        self.data = {
+            "title":"Merge Two Arrays",
+            "description": "Merge two arrays",
+            "difficulty":"EASY"
+
+        }
+
+    def test_admin_problem_create(self):
+        self.client.force_authenticate(user = self.admin)
+        response = self.client.post(self.all_problems_url, self.data)
+        id = response.data['id']
+        self.assertTrue(Problems.objects.filter(id = id).exists())
+        self.assertEqual(response.status_code,201 )
+
+
+    def test_user_problem_cannot_create(self):
+        self.client.force_authenticate(user = self.user)
+        response = self.client.post(self.all_problems_url, self.data)
+        self.assertEqual(response.status_code,403 )
+
+    def test_authenticated_user_problems_list(self):
+        self.client.force_authenticate(user = self.user)
+        response = self.client.get(self.all_problems_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_unauthenticated_user_problems_list(self):
+        self.client.force_authenticate(user = None)
+        response = self.client.get(self.all_problems_url)
+        self.assertEqual(response.status_code, 401)
--- backend/apps/users/tests.py
+++ backend/apps/users/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
-
+from rest_framework.test import APIClient 
+from .models import User
+from django.urls import reverse
 # Create your tests here.
+
+class UserAuthTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.login_url = reverse('login')
+        self.register_url = reverse('register')
+        self.data = {
+            "email":"test@test.com",
+            "username":"TestUser",
+            "password":"testpassword"
+        }
+
+
+    def test_register_creates_user(self):
+        response = self.client.post(self.register_url,self.data)
+
+        self.assertEqual(response.status_code,201)    
+        self.assertTrue(User.objects.filter(email = "test@test.com").exists())
+
+    def test_register_duplicate_email(self):
+        response1 = self.client.post(self.register_url, self.data)
+        self.assertEqual(response1.status_code,201)   
+
+        response2 = self.client.post(self.register_url, self.data)
+        self.assertEqual(response2.status_code,400 )       
+
+    def test_login_success(self):
+        self.client.post(self.register_url,self.data)
+        response = self.client.post(self.login_url, self.data)
+        self.assertEqual(response.status_code,200)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+        
+    def test_login_wrong_password(self):
+        self.client.post(self.register_url,self.data)
+        data2 =  {
+            "email":"test@test.com",
+            "username":"TestUser",
+            "password":"thisiswrongpassword"
+        }
+        response = self.client.post(self.login_url,data2)  
+        self.assertEqual(response.status_code, 400)      
