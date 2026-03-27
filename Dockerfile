6cc802f476e692619eb328d37a36f7a711b8d9f6
ANKISH1 <ankishchaudhary51@gmail.com>
2026-03-27 16:28:19 +0530

    feat:docker setup with celery and redis

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.11-slim
+
+WORKDIR /app
+COPY backend/requirements.txt .
+RUN pip install -r requirements.txt
+COPY backend/ .
+
+CMD ["gunicorn", "core.wsgi:application", "--bind", "0.0.0.0:8000"]
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -147,3 +147,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     )
 }
+
+#Celery Settings
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
