77686f3250f7f0821207e328b9cc476a93eb2dc6
ANKISH1 <ankishchaudhary51@gmail.com>
2026-04-11 00:41:58 +0530

    feat:Axios response interceptor for token refresh

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -12,5 +12,33 @@ api.interceptors.request.use((config) => {
     }
     return config
 })
+api.interceptors.response.use(
+    (response)=> response,
+
+    async (error)=>{
+        const original = error.config
+
+        if (error.response?.status ===401 && !original._retry){
+            original._retry = true
+
+            try{
+                const refresh = localStorage.getItem('refresh')
+                const res = await axios.post(
+                    `${import.meta.env.VITE_API_URL}/auth/token/refresh/`,
+                    {refresh}
+                    
+                )
+                localStorage.setItem('access', res.data.access)
+                original.headers.Authorization = `Bearer ${res.data.access}`
+                return api (original)
+            }
+            catch(err){
+                localStorage.clear()
+                window.location.href = '/login'
+            }
+        }
+        return Promise.reject(error)
+    }
+)    
 
 export default api
